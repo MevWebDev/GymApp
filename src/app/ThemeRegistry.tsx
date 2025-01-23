80500dfac798bb8bed855333f880020f9abdff5b
MevWebDev <szymongrysiewicz@gmail.com>
2025-01-23 20:45:53 +0100

    MUI Theme

---
+++ src/app/ThemeRegistry.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { createTheme, ThemeOptions, ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ReactNode } from "react";
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: "#ffffff",
+    },
+    secondary: {
+      main: "#000000",
+    },
+  },
+  typography: {
+    allVariants: {
+      color: "#262626",
+    },
+    h1: {
+      fontSize: "2.5rem",
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: "1.75rem",
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: "1.25rem",
+      fontWeight: 600,
+    },
+
+    fontSize: 15,
+  },
+};
+
+const theme = createTheme(themeOptions);
+
+export default function ThemeRegistry({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
