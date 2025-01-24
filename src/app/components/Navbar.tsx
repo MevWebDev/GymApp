81824199a808b1dfc11ebb88084c7ecf897db960
MevWebDev <szymongrysiewicz@gmail.com>
2025-01-25 00:46:35 +0100

    responsive navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,6 +3,10 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Link from "next/link";
+import HomeIcon from "@mui/icons-material/Home";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import ExploreIcon from "@mui/icons-material/Explore";
+import BackupTableIcon from "@mui/icons-material/BackupTable";
 
 export default function ButtonAppBar() {
   return (
@@ -11,31 +15,59 @@ export default function ButtonAppBar() {
         flexGrow: 1,
         display: "flex",
         justifyContent: "space-between",
-        mx: 12,
+        alignItems: "center",
+        px: { xs: 2, md: 5 },
         my: 2,
       }}
     >
-      <Box sx={{ display: "flex", gap: 5 }}>
+      <Box
+        sx={{
+          display: "flex",
+          gap: { xs: 2, md: 5 },
+          alignItems: "center",
+          justifySelf: "flex-end",
+        }}
+      >
         <Link href="/home">
-          <Typography variant="h6" component="div">
+          <Typography
+            sx={{ display: { xs: "none", md: "block" } }}
+            variant="h6"
+            component="div"
+          >
             Home
           </Typography>
+          <HomeIcon sx={{ display: { xs: "block", md: "none" } }} />
         </Link>
 
         <Link href="/exercises">
-          <Typography variant="h6" component="div">
+          <Typography
+            sx={{ display: { xs: "none", md: "block" } }}
+            variant="h6"
+            component="div"
+          >
             Exercises
           </Typography>
+          <FitnessCenterIcon sx={{ display: { xs: "block", md: "none" } }} />
         </Link>
         <Link href="/explore">
-          <Typography variant="h6" component="div">
+          <Typography
+            sx={{ display: { xs: "none", md: "block" } }}
+            variant="h6"
+            component="div"
+          >
             Explore
           </Typography>
+          <ExploreIcon sx={{ display: { xs: "block", md: "none" } }} />
         </Link>
         <Link href="/library">
-          <Typography variant="h6" component="div">
+          <Typography
+            sx={{ display: { xs: "none", md: "block" } }}
+            variant="h6"
+            component="div"
+          >
             Library
           </Typography>
+          <BackupTableIcon sx={{ display: { xs: "block", md: "none" } }} />
         </Link>
       </Box>
       <Button color="inherit">Login</Button>
