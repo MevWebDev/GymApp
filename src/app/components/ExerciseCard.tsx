1c1412f0c94d4f876a49479387764e281ec9f4c0
MevWebDev <szymongrysiewicz@gmail.com>
2025-01-25 00:46:13 +0100

    exercise card for showing exercise

---
+++ src/app/components/ExerciseCard.tsx
@@ -0,0 +1,38 @@
+import { Box, Typography } from "@mui/material";
+import { Exercise } from "../types";
+import Link from "next/link";
+
+export default function ExerciseCard({ exercise }: { exercise: Exercise }) {
+  return (
+    <Link href={`/exercises/${exercise.id}`} key={exercise.id}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          border: "1px solid ",
+          borderColor: "primary.light",
+          borderRadius: 4,
+          p: 4,
+          maxWidth: 400,
+          textOverflow: "ellipsis",
+          height: "100%",
+        }}
+      >
+        <img src={exercise.gifUrl} alt={exercise.name} />
+        <Typography
+          sx={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+          variant="h6"
+        >
+          {exercise.name}
+        </Typography>
+        <Typography>{exercise.bodyPart}</Typography>
+        <Typography>{exercise.target}</Typography>
+        <Typography>{exercise.equipment}</Typography>
+      </Box>
+    </Link>
+  );
+}
