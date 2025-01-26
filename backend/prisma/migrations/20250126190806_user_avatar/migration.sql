c0a38e613b9425208f46d7223cf440e1e8e8f844
MevWebDev <szymongrysiewicz@gmail.com>
2025-01-26 20:16:42 +0100

    schema

---
+++ backend/prisma/migrations/20250126190806_user_avatar/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "avatar" TEXT NOT NULL DEFAULT 'https://www.shutterstock.com/image-vector/vector-flat-illustration-grayscale-avatar-600nw-2264922221.jpg';
