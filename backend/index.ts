7afea166fa9f1a9b75159c27435cdff7715788b7
MevWebDev <szymongrysiewicz@gmail.com>
2025-02-02 14:30:03 +0100

    changed updating gifs on specific hour

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,7 @@ import exerciseRouter from "./routes/exercises";
 import workoutRouter from "./routes/workouts";
 import userRouter from "./routes/users";
 import cors from "cors";
+import updateGifUrls from "./scripts/updateGifs";
 
 const app = express();
 
@@ -17,3 +18,23 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+setInterval(async () => {
+  const now = new Date();
+  const hours = now.getUTCHours();
+  const minutes = now.getUTCMinutes();
+
+  const targetHourUTC = 18;
+  const targetMinuteUTC = 0;
+
+  if (hours === targetHourUTC && minutes === targetMinuteUTC) {
+    try {
+      await updateGifUrls();
+      console.log("GIF update job completed successfully.");
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error("Error during GIF update job:", error.message);
+      }
+    }
+  }
+}, 60 * 1000);
