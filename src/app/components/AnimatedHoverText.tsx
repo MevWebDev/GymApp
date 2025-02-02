4345e2eeb5093510ecf9ae87a7022e1b3a87f77d
MevWebDev <szymongrysiewicz@gmail.com>
2025-02-02 14:32:07 +0100

    animated hover text component

---
+++ src/app/components/AnimatedHoverText.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from "react";
+import { Typography } from "@mui/material";
+import { SxProps } from "@mui/material";
+
+export default function AnimatedHoverText({
+  children,
+  style,
+}: {
+  children: ReactNode;
+  style: SxProps;
+}) {
+  return (
+    <Typography
+      sx={{
+        position: "relative",
+        display: "inline-block",
+        color: "text.primary",
+        textDecoration: "none",
+        fontSize: { xs: 14, md: 18 },
+
+        "&::after": {
+          content: '""',
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          width: 0,
+          height: "2px",
+          backgroundColor: "currentColor",
+          transition: "width 0.3s ease",
+        },
+
+        "&:hover::after": {
+          width: "100%",
+        },
+        ...style,
+      }}
+    >
+      {children}
+    </Typography>
+  );
+}
