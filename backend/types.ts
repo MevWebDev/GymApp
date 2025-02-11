cf9b40fb759b0b113e502e3010eae51c0052f486
MevWebDev <szymongrysiewicz@gmail.com>
2025-02-11 21:38:39 +0100

    add more types

--- backend/types.ts
+++ backend/types.ts
@@ -3,13 +3,27 @@ import {
   WorkoutPlan,
   WorkoutPlanExercise,
   User,
+  CompletedWorkout,
+  CompletedWorkoutExercise,
 } from "@prisma/client";
 
-export type { Exercise, WorkoutPlan, WorkoutPlanExercise, User };
+export type {
+  Exercise,
+  WorkoutPlan,
+  WorkoutPlanExercise,
+  User,
+  CompletedWorkout,
+  CompletedWorkoutExercise,
+};
 
 export type fullWorkoutPlan = WorkoutPlan & {
   user: User;
   exercises: Array<WorkoutPlanExercise & { exercise: Exercise }>;
+};
+
+export type fullCompletedWorkout = CompletedWorkout & {
+  user: User;
+  exercises: Array<CompletedWorkoutExercise & { exercise: Exercise }>;
 };
 
 export type completeUser = User & {
