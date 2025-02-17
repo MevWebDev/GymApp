6812375e864421c567577927855a8f4a74a67ae4
MevWebDev <szymongrysiewicz@gmail.com>
2025-02-17 19:40:12 +0100

    fix port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -62,7 +62,7 @@ app.use("/api/workouts", workoutRouter);
 app.use("/api/users", userRouter);
 
 app.listen(process.env.PORT || 3001, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running on port ${process.env.PORT || 3001}`);
 });
 
 server.listen(process.env.PORT, () => {
